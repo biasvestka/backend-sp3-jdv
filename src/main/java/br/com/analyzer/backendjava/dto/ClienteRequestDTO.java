8421af6d5ec95bd0af4bc80a991b82aef8689562
Beatriz Svestka <89924879+biasvestka@users.noreply.github.com>
2024-09-14 12:11:25 -0300

    Update ClienteRequestDTO.java

--- src/main/java/br/com/analyzer/backendjava/dto/ClienteRequestDTO.java
+++ src/main/java/br/com/analyzer/backendjava/dto/ClienteRequestDTO.java
@@ -4,53 +4,11 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public class ClienteRequestDTO {
+public record ClienteRequestDTO(
 
-    @NotBlank(message = "O nome é obrigatório.")
-    @Size(min = 2, max = 100, message = "O nome deve ter entre 2 e 100 caracteres.")
-    private String nome;
+    @NotBlank(message = "O nome é obrigatório.") @Size(min = 2, max = 100, message = "O nome deve ter entre 2 e 100 caracteres.") String nome,
 
-    @NotBlank(message = "O email é obrigatório.")
-    @Email(message = "Formato de email inválido.")
-    private String email;
+    @NotBlank(message = "O email é obrigatório.") @Email(message = "Formato de email inválido.") String email,
 
-    @NotBlank(message = "A senha é obrigatória.")
-    @Size(min = 8, message = "A senha deve ter pelo menos 8 caracteres.")
-    private String password;
-
-    // Construtor padrão
-    public ClienteRequestDTO() {
-    }
-
-    // Construtor com parâmetros
-    public ClienteRequestDTO(String nome, String email, String password) {
-        this.nome = nome;
-        this.email = email;
-        this.password = password;
-    }
-
-    // Getters e Setters
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    @NotBlank(message = "A senha é obrigatória.") @Size(min = 8, message = "A senha deve ter pelo menos 8 caracteres.") String password) {
 }
